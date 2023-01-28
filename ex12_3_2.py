c0887bc0dcfda36d098290ba018a08ee05cd58eb
ElisaBluebell <selfishracer@hotmail.com>
2023-01-28 19:20:19 +0900

    230128 Elisa Bluebell

--- ex12_3_2.py
+++ ex12_3_2.py
@@ -8,36 +8,32 @@ from socket import *
 class ThreadServer(threading.Thread):
     def __init__(self, clientAddress, clientsocket):
         threading.Thread.__init__(self)
-        self.csocket = clientsocket  # 인스턴스 변수 정의
+        self.clientsocket = clientsocket  # 인스턴스 변수 정의
         print("New socket added: ", clientAddress)
 
     def run(self):
         while True:
-            data = self.csocket.recv(BUFSIZE)  # 데이터 수신
+            data = self.clientsocket.recv(BUFSIZE)  # 데이터 수신
             if not data:  # 연결이 종료되었음
                 break
             data = float(data.decode())  # 수신 데이터를 float 형으로 변환
             data = 9.0 / 5.0 * data + 32.0  # 화씨온도 계산
             data = '{:.1f}'.format(data)
-            self.csocket.send(data.encode())  # 화씨온도 전송
+            self.clientsocket.send(data.encode())  # 화씨온도 전송
 
 
-ECHO_PORT = 2500
+PORT = 2500
 BUFSIZE = 1024
 
 if len(sys.argv) > 1:
     port = int(eval(sys.argv[1]))
 else:
-    port = ECHO_PORT
+    port = PORT
 
 s = socket(AF_INET, SOCK_STREAM)
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 s.bind(('', port))
 s.listen(1)
-
-print("Waiting for connection from client")
-conn, (remotehost, remoteport) = s.accept()
-print('connected by', remotehost, remoteport)
 
 while True:
     s.listen(1)
