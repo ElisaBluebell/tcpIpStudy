205d8a51e8af26737d25049ae5afd4cfa3a0de5a
ElisaBluebell <selfishracer@hotmail.com>
2023-01-30 21:04:07 +0900

    230130 Elisa Bluebell

--- test.py
+++ test.py
@@ -1,70 +1,60 @@
-import socket
-from tkinter import *
-from tkinter.scrolledtext import ScrolledText
+# threading 모듈을 이용한 TCP 멀티 채팅 서버 프로그램
+
+from socket import *
 from threading import *
 
 
-class ChatClient:
-    def __init__(self, ip, port):
-        self.initialize_socket(ip, port)
-        self.initialize_gui()
-        self.listen_thread()
+class MultiChatServer:
 
-    def initialize_socket(self, ip, port):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.client_socket.bind((ip, port))
+    # 소켓을 생성하고 연결되면 accept_client() 호출
+    def __init__(self):
+        self.clients = []  # 접속된 클라이언트 소켓 목록
+        self.final_received_message = ""  # 최종 수신 메시지
+        self.s_sock = socket(AF_INET, SOCK_STREAM)
+        self.ip = '10.10.21.121'
+        self.port = 25000
+        self.s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.s_sock.bind((self.ip, self.port))
+        print("클라이언트 대기 중...")
+        self.s_sock.listen(100)
+        self.accept_client()
 
-    def send_chat(self):
-        senders_name = self.name_widget.get().strip() + ":"
-        data = self.enter_text_widget.get(1.0, 'end').strip()
-        message = (senders_name + data).encode('utf-8')
-        self.chat_transcript_area.insert('end', message.decode('utf-8') + '\n')
-        self.chat_transcript_area.yview(END)
-        self.client_socket.send(message)
-        self.enter_text_widget.delete(1.0, 'end')
-        return 'break'
-
-    def initialize_gui(self):
-        self.root = Tk()
-        fr = []
-        for i in range(0, 5):
-            fr.append(Frame(self.root))
-            fr[i].pack(fill=BOTH)
-
-        self.name_label = Label(fr[0], text='사용자 이름')
-        self.recv_label = Label(fr[1], text='수신 메시지:')
-        self.send_label = Label(fr[3], text='송신 메시지:')
-        self.send_btn = Button(fr[3], text='전송', command=self.send_chat)
-        self.chat_transcript_area = ScrolledText(fr[2], height=20, width=60)
-        self.enter_text_widget = ScrolledText(fr[4], height=5, width=60)
-        self.name_widget = Entry(fr[0], width=15)
-
-        self.name_label.pack(side=LEFT)
-        self.name_widget.pack(side=LEFT)
-        self.recv_label.pack(side=LEFT)
-        self.send_btn.pack(side=RIGHT, padx=20)
-        self.chat_transcript_area.pack(side=LEFT, padx=2, pady=2)
-        self.send_label.pack(side=LEFT)
-        self.enter_text_widget.pack(side=LEFT, padx=2, pady=2)
-
-    def listen_thread(self):
-        t = Thread(target=self.recieve_message, args=(self.client_socket, ))
-        t.start()
-
-    def recieve_message(self, so):
+    # 연결 클라이언트 소켓을 목록에 추가하고 스레드를 생성하여 데이터를 수신한다.
+    def accept_client(self):
         while True:
-            data, addr = so.recvfrom(256)
-            if not data:  # 연결이 종료됨
-                break
-            self.chat_transcript_area.insert('end', data.decode('utf-8') + '\n')
-            self.chat_transcript_area.yview(END)
-        so.close()
+            client = c_socket, (ip, port) = self.s_sock.accept()
+            if client not in self.clients:
+                self.clients.append(client)  # 접속된 소켓을 목록에 추가
+            print(ip, ':', str(port), ' 가 연결되었습니다.')
+            cth = Thread(target=self.receive_messages, args=(c_socket,))  # 수신 스레드
+            cth.start()  # 스레드 시작
+
+    # 데이터를 수신하여 모든 클라이언트에게 전송한다. 이거 안댐
+    def receive_messages(self, c_socket):
+        while True:
+            try:
+                incoming_message = c_socket.recv(256)
+                if not incoming_message:  # 연결이 종료됨
+                    break
+            except:
+                continue
+            else:
+                self.final_received_message = incoming_message.decode('utf-8')
+                print(self.final_received_message)
+                self.send_all_clients(c_socket)
+        c_socket.close()
+
+    # 송신 클라이언트를 제외한 모든 클라이언트에게 메시지 전송
+    def send_all_clients(self, senders_socket):
+        for client in self.clients:  # 목록에 있는 모든 소켓에 대해
+            socket, (ip, port) = client
+            if socket is not senders_socket:  # 송신 클라이언트는 제외
+                try:
+                    socket.sendall(self.final_received_message.encode())
+                except:  # 연결 종료
+                    self.clients.remove(client)  # 소켓 제거
+                    print("{}, {} 연결이 종료되었습니다.".format(ip, port))
 
 
 if __name__ == "__main__":
-    ip = input("server IP addr: ")
-    if ip == '':
-        ip = '127.0.0.1'
-    port = 2500
-    ChatClient(ip, port)
-    mainloop()
+    MultiChatServer()
